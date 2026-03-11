00999be3bff7a67f4ddc11e2005d5f5ab175a0ab
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 02:04:00 +0800

    bigger number

--- equal_numbers.py
+++ equal_numbers.py
@@ -1,3 +1,7 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
+if first_number > second_number:
+    print("the bigger number is ", first_number)
+elif first_number < second_number:
+    print("the bigger number is ", second_number)
