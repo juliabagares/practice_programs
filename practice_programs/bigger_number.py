4023acaa8597025642bd008faa1c72e97d65a9cb
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 01:19:07 +0800

    bigger number

--- practice_programs/bigger_number.py
+++ practice_programs/bigger_number.py
@@ -1,3 +1,6 @@
 first_number = float(input("First number: "))
 second_number = float(input("Second number: "))
 
+bigger_number = max(first_number, second_number)
+
+print("the bigger number is ", bigger_number)
