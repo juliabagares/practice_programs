cf3793e018264d15802ff7c62c50760e7ca84930
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 16:04:00 +0800

    remainder of the two numbers

--- num1_divided_by_num2.py
+++ num1_divided_by_num2.py
@@ -1,2 +1,5 @@
 first_number = float(input("First number: "))
 second_number = float(input("Second number: "))
+
+remainder = first_number % second_number
+print("The remainder of two numbers is ", remainder)
