c7229ff457c3263b96d23358fdde9a4ade7a33ea
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 12:17:22 +0800

    product of two numbers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = float(input("first number: "))
 second_number = float(input("second number: "))
+
+product = first_number * second_number
+print("the product of the two numbers is ", product)
