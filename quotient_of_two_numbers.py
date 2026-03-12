c0d24b70a51ba6ada11eb3a7c26a9951f60662f0
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 12:42:57 +0800

    quotient of the two numbers

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -2,5 +2,5 @@ first_number = float(input("the first number: "))
 second_number = float(input("the second number: "))
 
 quotient = first_number / second_number
-remainder = first_number % second_number
 
+print("the quotient of the two numbers is ", quotient)
