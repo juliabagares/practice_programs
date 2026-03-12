5547e187c72c07a9b17c80552b865e73dce118fc
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 15:54:27 +0800

    difference

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+difference = first_number - second_number
+print("The difference of two number is ", difference)
