73ae0a28443789b85b09123b6fecbfe682525d50
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 12:53:29 +0800

    exponential

--- raised_to_the_second_number.py
+++ raised_to_the_second_number.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+exponential = first_number ** second_number
+print("the exponential of the two numbers is ", exponential)
