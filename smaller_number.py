d2069430b828de49857f77883283b70ec1d2df16
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 15:42:03 +0800

    smaller number

--- smaller_number.py
+++ smaller_number.py
@@ -1,3 +1,5 @@
 first_number = float(input('Enter first number: '))
 second_number = float(input('Enter second number: '))
 
+smaller_number = min(first_number, second_number)
+print("the smaller number is ", smaller_number)
