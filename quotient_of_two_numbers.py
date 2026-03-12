33e78535559b3bfb7f8cd07f4717e15718a5b498
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 12:41:58 +0800

    quotient formula

--- quotient_of_two_numbers.py
+++ quotient_of_two_numbers.py
@@ -1,2 +1,6 @@
 first_number = float(input("the first number: "))
 second_number = float(input("the second number: "))
+
+quotient = first_number / second_number
+remainder = first_number % second_number
+
