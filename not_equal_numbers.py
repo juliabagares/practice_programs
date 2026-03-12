29e326cadb7944068f1aa81c6ea858734951ef9d
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 15:49:10 +0800

    numbers are not equal

--- not_equal_numbers.py
+++ not_equal_numbers.py
@@ -1,2 +1,7 @@
 first_number = float(input("First number: "))
 second_number = float(input("Second number: "))
+
+if first_number != second_number:
+    print("The numbers are not equal.")
+elif first_number == second_number:
+    print("The numbers are equal.")
