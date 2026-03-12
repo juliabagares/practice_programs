b22826c395e1a06b4ab7775d7f92557d9dfdefa9
Julia Bagares <juliabagares2007@gmail.com>
2026-03-12 15:58:22 +0800

    quorient without decimals

--- quotient_without_decimals.py
+++ quotient_without_decimals.py
@@ -1,2 +1,5 @@
 first_number = int(input("First number: "))
 second_number = int(input("Second number: "))
+
+quotient = first_number // second_number
+print("The quotient of two numbers is ", quotient)
