d4eca59be4f1d68fb8db3ced038913d56f0efefb
Julia Bagares <juliabagares2007@gmail.com>
2026-03-20 13:31:55 +0800

    result

--- first_number_minus_remaining_numbers.py
+++ first_number_minus_remaining_numbers.py
@@ -6,3 +6,5 @@ print("The first number is ", first_number)
 sum_of_remaining_numbers = sum(numbers[1:])
 print("The sum of all the remaining numbers is ", sum_of_remaining_numbers)
 
+result = first_number - sum_of_remaining_numbers
+print("The result is ", result)
