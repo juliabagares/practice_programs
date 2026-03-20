01a178768d214a753ec9ae461fa84f8c04fdb3b6
Julia Bagares <juliabagares2007@gmail.com>
2026-03-20 16:22:13 +0800

    unique or duplicate

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,6 +1,14 @@
 numbers=[]
 
 while True:
-    number=int(input("enter the number: "))
-    numbers.append(number)
-    
+    try:
+        number=(int(input("enter a number: ")))
+        numbers.append(number)
+
+        if numbers.count(number) == 1:
+            print("unique")
+        else:
+            print("duplicate")
+
+    except ValueError:
+        break
