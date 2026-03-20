0d65ca4fae401c6bb90a9c1980845d83cb13a113
Julia Bagares <juliabagares2007@gmail.com>
2026-03-20 14:39:30 +0800

    the numbers between the two numbers

--- numbers_between_two_numbers.py
+++ numbers_between_two_numbers.py
@@ -2,3 +2,6 @@ first_number = int(input("First number: "))
 second_number = int(input("Second number: "))
 
 print(f"the numbers between {first_number} and {second_number} are:")
+
+for i in range(first_number, second_number + 1):
+    print(i)
