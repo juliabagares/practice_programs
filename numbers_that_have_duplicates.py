07aa14239b4c1a7938ca9e3b0cd4b9956561c97b
Julia Bagares <juliabagares2007@gmail.com>
2026-03-20 17:07:22 +0800

    one of each duplicates

--- numbers_that_have_duplicates.py
+++ numbers_that_have_duplicates.py
@@ -1,3 +1,5 @@
 numbers=[int(input(f"number {i+1}:")) for i in range (10)]
 
 duplicates = [n for n in numbers if numbers.count(n) > 1]
+duplicates = list(dict.fromkeys(duplicates))
+print("duplicate numbers:", duplicates)
