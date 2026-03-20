07734cce6b27ef80ce9b077a839af0e6f3ab6866
Julia Bagares <juliabagares2007@gmail.com>
2026-03-20 15:45:28 +0800

    no duplicate first

--- numbers_with_duplicates.py
+++ numbers_with_duplicates.py
@@ -1,4 +1,3 @@
 numbers = [int(input(f"number{i+1}:")) for i in range(10)]
 
-unique_numbers = list(set(numbers))
-print("numbers (remove duplicate, keep first):", unique_numbers)
+no_duplicate_first = []
