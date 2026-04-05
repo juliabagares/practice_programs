e50f91a9685d6bcb4b661bed5c6659d08f4a27f9
Julia Bagares <juliabagares2007@gmail.com>
2026-04-06 00:26:01 +0800

    spaces

--- without_using_center.py
+++ without_using_center.py
@@ -1,3 +1,7 @@
 text = input("Enter a string: ")
 target_length = int(input("length: "))
 
+total_spaces = target_length - len(text)
+left_spaces = total_spaces // 2
+right_spaces = total_spaces - left_spaces
+
