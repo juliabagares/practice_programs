830a5debae731916593bccf074236c8dc81f7893
Julia Bagares <juliabagares2007@gmail.com>
2026-04-06 02:01:29 +0800

    convert to uppercase

--- without_using_title.py
+++ without_using_title.py
@@ -6,4 +6,7 @@ for char in text:
     if char == " ":
         title_text += char
         start_of_word = True
-        
+    elif start_of_word:
+        if 'a' <= char <= 'z':
+            title_text += chr(ord(char) - 32)
+    
