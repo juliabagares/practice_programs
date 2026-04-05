8ad780f6048c21e0dd63aa687908a866cffd6163
Julia Bagares <juliabagares2007@gmail.com>
2026-04-05 23:43:29 +0800

    print in lower case

--- without_using_lower.py
+++ without_using_lower.py
@@ -4,4 +4,7 @@ lower_text = ""
 for character in text:
     if 'A' <= character <= 'Z':
         lower_text += chr(ord(character)+32)
-        
+    else:
+        lower_text += character
+
+print(lower_text)
