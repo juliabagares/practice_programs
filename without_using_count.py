227a7f4e8f7878f74e1083b52070bd46a22fe143
Julia Bagares <juliabagares2007@gmail.com>
2026-04-09 00:26:34 +0800

    word count

--- without_using_count.py
+++ without_using_count.py
@@ -2,3 +2,6 @@ text = input("enter text: ")
 word = input("enter word: ")
 count = 0
 
+for index in range(len(text) - len(word) + 1):
+    if text[index:index + len(word)] == word:
+        count += 1
