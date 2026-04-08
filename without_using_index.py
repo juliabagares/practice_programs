712a7166da0b4e953d01f25102401778a31bf29e
Julia Bagares <juliabagares2007@gmail.com>
2026-04-09 00:43:27 +0800

    first location of the function

--- without_using_index.py
+++ without_using_index.py
@@ -1,2 +1,7 @@
 text = input("enter text: ")
 word = input("enter word: ")
+
+for index in range(len(text) - len(word) + 1):
+    if text[index:index + len(word)] == word:
+        print(text)
+        break
