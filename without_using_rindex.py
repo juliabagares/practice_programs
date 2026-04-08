704bbc94acd3861a116fab6e9b62d343c697804d
Julia Bagares <juliabagares2007@gmail.com>
2026-04-09 00:54:15 +0800

    position from the end

--- without_using_rindex.py
+++ without_using_rindex.py
@@ -1,2 +1,7 @@
 text = input("enter string: ")
 word = input("enter word: ")
+
+for index in range(len(text) - len(word), -1, -1):
+    if text[index:index + len(word)] == word:
+        print(index)
+        break
