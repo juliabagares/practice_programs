df24e64f866d0147cc02be30fce2295f8ddfe82f
Julia Bagares <juliabagares2007@gmail.com>
2026-04-30 09:00:23 +0800

    sys.exit()

--- calculator_app.py
+++ calculator_app.py
@@ -1,19 +1,4 @@
 import sys
 
-class Math:
-    @staticmethod
-    def calculate(num1, num2, operation):
-        if operation == '1':
-            return num1 + num2
-        elif operation == '2':
-            return num1 - num2
-        elif operation == '3':
-            return num1 * num2
-        elif operation == '4':
-            if num2 == 0:
-                raise ZeroDivisionError ("Math error")
-            return num1 / num2
-        else:
-            raise ValueError ("Invalid operation")
 
 
